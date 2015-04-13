d959bb4833728c4c926e9dabb288c49b52cf9a35
JoseOm07 <beto_bcw_09@hotmail.com>
2015-04-13 13:54:30 -0500

    99% listo

---
+++ Indice_denso/Disco.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Disco{
+	
+	public static void main(String[] Joseses){
+		Scanner sc = new Scanner(System.in);
+		int option;
+		
+		try { //Previene cuando el usuario escribe cualquier cosa menos un número.
+			do{
+				showMenu();
+				option = sc.nextInt();
+				setMenu(option);
+			}while(option != 5);
+		} catch (Exception e) {
+			System.out.println("Caracter inválido");
+		}
+	}
+    
+	private static void showMenu(){
+		System.out.print("Con el numero correspondiente, elija una opcion del menu: "+ "\n" +
+							"1) Crear un Registro"+ "\n"+
+							"2) Eliminar un Registro"+"\n"+
+                                                        "3) Buscar un Registro(Busqueda Lineal)"+"\n"+
+							"4) Imprimir todos los Registros"+"\n"+
+							"5) Salir del programa"+"\n"+
+                        "6) Crear 100mil registros Aleatoreamente(1000 registros por 100 entradas)"+"\n"+
+							"====== Opción: ");
+	}
+    
+	public static void setMenu(int option){
+		try {
+// metadatos del archivo de datos y del archivo �ndice
+                    File datos = new File( "Archivo.Datos" );
+                    File denso = new File( "Indice.Denso" );
+            
+                    // handlers para manipular el contenido de los archivos
+                    RandomAccessFile archivoRaF = new RandomAccessFile( datos, "rw" );
+                    RandomAccessFile indiceRaF = new RandomAccessFile( denso, "rw" );
+                     // archivo indexado usando una clave de b�squeda de 20 bytes
+			Archivo archivo = new Archivo( archivoRaF, indiceRaF );
+            
+			Registro registro;
+			
+			Scanner sc = new Scanner(System.in);
+			if(option == 1){
+				System.out.println("--------------------------------------------------------------");
+				System.out.print("Introduzca el nombre de la sucursal: ");
+				String suc = sc.nextLine();
+				System.out.print("Introduzca el número de cuenta: ");
+				int num = sc.nextInt();
+				sc.nextLine();
+				System.out.print("Introduzca el nombre del titular: ");
+				String nom = sc.nextLine();
+				System.out.print("Introduzca la cantidad de la cuenta: ");
+				double sal = sc.nextDouble();
+				registro = new Registro(suc, num, nom, sal);
+				archivo.insertar(registro);
+                                System.out.println("-------------------------Registro Agregado-------------------------");
+			}else if(option == 2){
+				System.out.println("--------------------------------------------------------------");
+				//archivo.mostrar();
+				System.out.println("Introduzca el nombre de la Sucursal del registro a eliminar");
+                                System.out.print("(Se eliminara el primer registro encontrado de la sucursal): ");
+				String suc = sc.nextLine();
+                                System.out.println("--------------------------------------------------------------");
+                                if(archivo.borrar(suc))
+                                    System.out.println("El registro se eliminó correctamente");
+                                else
+                                    System.out.println("No existe el registro");
+                                System.out.println("--------------------------------------------------------------");
+                        }else if(option == 3){
+				System.out.println("--------------------------------------------------------------");
+				System.out.print("Introduzca el nombre de la Sucursal: ");
+				String suc = sc.nextLine();
+                                System.out.print("Introduzca el numero de cuenta: ");
+				int num = sc.nextInt();
+                                System.out.println("--------------------------------------------------------------");
+				archivo.busquedaLineal(suc,new Registro(),num);
+                                System.out.println("--------------------------------------------------------------");
+			}else if(option == 4){
+				System.out.println("--------------------------------------------------------------");
+				archivo.mostrar();
+                                System.out.println("--------------------------------------------------------------");
+			}else if(option == 5){
+				System.out.println("--------------------------------------------------------------");
+				System.out.println("Salir, Adios");
+				archivo.cerrar();
+			}else if(option == 6){
+				System.out.println("--------------------------------------------------------------");
+				System.out.println("Creando registros");
+				for( int num = 1, i = 1; i <= 100; i++ ) { //Numero de Surcursales
+                                    for( int j = 1; j <= 1000; j++ ) { //Numero de clientes
+                                        for( int k = 1; k <= 1; k++, num++ ) {
+                                            String suc = "Sucursal " + String.format( "%3d", i );
+                                            String nom = "Cliente " + j;
+                                            double salMin = 100.0, salMax = 30000.6;
+                                            double sal = Math.random() * (salMax - salMin) + salMin;
+                                            archivo.insertar( new Registro( suc, num, nom, sal ) );
+                    }
+                }
+            }
+                                System.out.println("Registros creados");
+			}else{
+				System.out.println("--------------------------------------------------------------");
+				System.out.println("Error, Opción invalida");
+                                System.out.println("--------------------------------------------------------------");
+			}
+		} catch( Exception e ) {
+            
+			System.out.println( "IOException:" );
+			e.printStackTrace();
+                        
+		}
+	}
+}
